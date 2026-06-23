8302dab811fae993cc0033cea39b9162eab87eba
Alexey <life@kylc.ru>
2026-06-23 13:42:35 +0300

    fix migr.

--- backend/src/main/resources/db/migration/V002__change_schema.sql
+++ backend/src/main/resources/db/migration/V002__change_schema.sql
@@ -1,5 +1,19 @@
-ALTER TABLE product ADD COLUMN price DOUBLE PRECISION;
-ALTER TABLE orders ADD COLUMN date_created DATE DEFAULT current_date;
+DROP table IF EXISTS order_product;
 
-DROP TABLE product_info;
-DROP TABLE orders_date;
+ALTER TABLE product ADD PRIMARY KEY (id);
+ALTER TABLE product ADD COLUMN price DOUBLE PRECISION;
+ALTER TABLE orders ADD PRIMARY KEY (id);
+ALTER TABLE orders ADD COLUMN date_created DATE;
+
+CREATE TABLE order_product (
+    order_id BIGINT,
+    product_id BIGINT,
+    quantity INTEGER,
+    
+    PRIMARY KEY (order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+DROP TABLE IF EXISTS product_info;
+DROP TABLE IF EXISTS orders_date;
--- backend/src/main/resources/db/migration/V004__create_index.sql
+++ backend/src/main/resources/db/migration/V004__create_index.sql
@@ -1,2 +1,2 @@
-CREATE INDEX idx_order_product_order_id ON order_product(order_id);
-CREATE INDEX idx_orders_status_date_created ON orders(status, date_created);
+CREATE INDEX order_product_order_id_idx ON order_product(order_id);
+CREATE INDEX orders_status_date_idx ON orders(status, date_created);
